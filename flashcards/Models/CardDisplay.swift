0fadd443bbd3c1c3986d4eedbee350e46797099f
Harris <harris@h4z.dev>
2024-05-14 11:06:07 +1000

    animations are nicer

--- flashcards/Models/CardDisplay.swift
+++ flashcards/Models/CardDisplay.swift
@@ -26,25 +26,30 @@ struct CardDisplayFront: View {
     
     var body: some View {
         ZStack{
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundStyle(color)
+            VStack{
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(color)
+                    .rotation3DEffect(
+                        Angle(degrees: flipped && index == deckModel.deck.cards.count-1 ? Double(90) : Double()),
+                        axis: (x: Double(0.0), y: Double(1.0), z: Double(0.0))
+                    )
+            //        .opacity(flipped && index == deckModel.deck.cards.count-1  ? 0 : 1)
+                    .animation(flipped && index == deckModel.deck.cards.count-1  ? .linear(duration: 0.15) : .linear(duration:0.15).delay(0.15), value: flipped && index == deckModel.deck.cards.count-1 )
+//                    .rotationEffect(Angle(degrees: Double(index) * 2 - 10))
+                    .offset(x: CGFloat(abs((deckModel.deck.cards.count - 1) - index) * 5), y: CGFloat(index * -3))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                    .onReceive(deckModel.$flipped, perform: { flipped in
+                        self.flipped = flipped
+                    })
+                    .onReceive(deckModel.$currentCard, perform: { currentCard in
+                        withAnimation(.easeInOut){
+                            self.currentIndex = currentCard.index
+                        }                    })
+            }
             Text(text)
+                .foregroundStyle(flipped ? Color(.clear) : Color(.black))
+                .animation(flipped ? .linear : .linear.delay(0.15))
         }
-        .rotation3DEffect(
-            Angle(degrees: flipped && index == deckModel.deck.cards.count-1 ? Double(90) : Double()),
-            axis: (x: Double(0.0), y: Double(1.0), z: Double(0.0))
-        )
-//        .opacity(flipped && index == deckModel.deck.cards.count-1  ? 0 : 1)
-        .animation(flipped && index == deckModel.deck.cards.count-1  ? .linear(duration: 0.15) : .linear(duration:0.15).delay(0.15), value: flipped && index == deckModel.deck.cards.count-1 )
-        .rotationEffect(Angle(degrees: Double(index) * 2 - 10))
-        .offset(x: CGFloat(index * 5 - 20), y: CGFloat(index * -3))
-        .shadow(color: .gray, radius: 2, x: 0, y: 2)
-        .onReceive(deckModel.$flipped, perform: { flipped in
-            self.flipped = flipped
-        })
-        .onReceive(deckModel.$currentCard, perform: { currentCard in
-            self.currentIndex = currentCard.index
-        })
     }
 }
 struct CardDisplayBack: View {
@@ -63,25 +68,31 @@ struct CardDisplayBack: View {
     }
     
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundStyle(color)
+        ZStack {
+            VStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(color)
+                    .rotation3DEffect(
+                        Angle(degrees: flipped && index == deckModel.deck.cards.count-1  ? Double() : Double(90)),
+                        axis: (x: Double(0.0), y: Double(1.0), z: Double(0.0))
+                    )
+            //        .opacity( flipped && index == deckModel.deck.cards.count-1  ? 1 : 0)
+                    .animation(flipped && index == deckModel.deck.cards.count-1  ? .linear(duration: 0.15).delay(0.15) : .linear(duration: 0.15).delay(0.15), value: flipped && index == deckModel.deck.cards.count-1 )
+//                    .rotationEffect(Angle(degrees: Double(index) * 2 - 10))
+                    .offset(x: CGFloat(abs((deckModel.deck.cards.count - 1) - index) * 5), y: CGFloat(index * -3))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                    .onReceive(deckModel.$flipped, perform: { flipped in
+                        self.flipped = flipped
+                    })
+                    .onReceive(deckModel.$currentCard, perform: { currentCard in
+                        withAnimation(.easeInOut){
+                            self.currentIndex = currentCard.index
+                        }
+                    })
+            }
             Text(text)
+                .foregroundStyle(flipped ? Color(.black) : Color(.clear))
+                .animation(flipped ? .linear.delay(0.15) : .linear)
         }
-        .rotation3DEffect(
-            Angle(degrees: flipped && index == deckModel.deck.cards.count-1  ? Double() : Double(90)),
-            axis: (x: Double(0.0), y: Double(1.0), z: Double(0.0))
-        )
-//        .opacity( flipped && index == deckModel.deck.cards.count-1  ? 1 : 0)
-        .animation(flipped && index == deckModel.deck.cards.count-1  ? .linear(duration: 0.15).delay(0.15) : .linear(duration: 0.15), value: flipped && index == deckModel.deck.cards.count-1 )
-        .rotationEffect(Angle(degrees: Double(index) * 2 - 10))
-        .offset(x: CGFloat(index * 5 - 20), y: CGFloat(index * -3))
-        .shadow(color: .gray, radius: 2, x: 0, y: 2)
-        .onReceive(deckModel.$flipped, perform: { flipped in
-            self.flipped = flipped
-        })
-        .onReceive(deckModel.$currentCard, perform: { currentCard in
-            self.currentIndex = currentCard.index
-        })
     }
 }
