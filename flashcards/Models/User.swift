7b83c83423345f7e3b725a0f9a411e7b201ce675
microcoded <jacob@goodrid.ge>
2024-05-16 17:32:51 +1000

    Begin model documentation

--- flashcards/Models/User.swift
+++ flashcards/Models/User.swift
@@ -5,13 +5,15 @@
 
 import Foundation
 
+/// Structure to represent a user of Flashcards
 struct User: Identifiable, Codable {
-    let id: String
-    let fullname: String
-    let email: String
-    let googleSignIn: Bool
-    let emailSignIn: Bool
+    let id: String              /// Unique ID of the user
+    let fullname: String        /// Full name of the user
+    let email: String           /// Email of the user
+    let googleSignIn: Bool      /// Is the user signed in with Google OAuth? (Currently always false)
+    let emailSignIn: Bool       /// Is the user signed in with an Email address?
     
+    /// The initials of the user based on their provided full name.
     var initals: String {
         let formatter = PersonNameComponentsFormatter()
         if let components = formatter.personNameComponents(from: fullname) {
@@ -22,6 +24,7 @@ struct User: Identifiable, Codable {
     }
 }
 
+/// Mock user used for testing purposes.
 extension User {
     static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Harris Vandenberg", email: "testing123@gmail.com", googleSignIn: false, emailSignIn: true)
 }
